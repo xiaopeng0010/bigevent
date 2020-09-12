5210c50e872405dcd615b0e068f1c98a6d58715b
lv597peng <l>
2020-09-12 15:33:36 +0800

    no message

--- assets/js/login1.js
+++ assets/js/login1.js
@@ -9,6 +9,7 @@ $(function () {
     })
     // 从layui中获取对象
     var form = layui.form
+    var layer = layui.layer
     //通过form.verify()函数自定义校验规则
     form.verify({
         //自定义一个pwd校验规则
@@ -27,13 +28,34 @@ $(function () {
     });
     $('#form_reg').on('submit', function (e) {
         e.preventDefault();
-        $.post('http://ajax.frontend.itheima.net/api/reguser', {
-            username: $('#form_reg [name = uesrname]').val(), password: $('#form_reg [name = password]').val()
+        $.post('/api/reguser', {
+            username: $('#form_reg [name=username]').val(),
+            password: $('#form_reg [name=password]').val()
         }, function (res) {
+            console.log(res);
             if (res.status !== 0) {
                 return layer.msg(res.message)
-                }
-                console.log('注册成功');
+            }
+            layer.msg('注册成功，请登录！')
+            $('#link_login').click()
         })
     })
+
+
+    $('#form_login').submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            url: '/api/login',
+            method: 'POST',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('登录失败,请检查用户名和密码是否正确')
+                }
+                layer.msg('登录成功')
+            }
+
+        })
+    })
+
 })
